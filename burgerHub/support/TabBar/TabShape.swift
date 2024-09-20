1a8d514b6b6bc138485cb0ca539acacf05094f22
vanokvakhadze <vkvakhadze35@gmail.com>
2024-09-20 18:14:39 +0400

    modify tab bar

---
+++ burgerHub/support/TabBar/TabShape.swift
@@ -0,0 +1,18 @@
+//
+//  TabShape.swift
+//  burgerHub
+//
+//  Created by vano Kvakhadze on 20.09.24.
+//
+
+import SwiftUI
+
+struct TabShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabShape()
+}
